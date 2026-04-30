f02af61b1f361ea01083dab4bcf580ebb9da8b1d
ayshaaktersaima1 <ayshaaktersaima1.com>
2026-04-30 23:12:20 +0600

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <div className='h-[60vh] bg-amber-300'></div>
+
+            <div>
+                <h1>Featured tiles</h1>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <div className='h-[20vh] bg-black'></div>
+        </div>
+    );
+};
+
+export default Footer;
